6a6f1d06b544f74a302521379d3719cbf088c135
Emilio <emil.soto@profesor.duoc.cl>
2025-05-09 12:32:09 -0400

    Implementacion Clase Vendedor

---
+++ Vendedor.java
@@ -0,0 +1,9 @@
+public class Vendedor {
+    int codigoVendedor;
+    String nombreVendedor;
+
+    Vendedor(int codigoVendedor, String nombreVendedor){
+        this.codigoVendedor = codigoVendedor;
+        this.nombreVendedor = nombreVendedor;
+    }
+}
--- holaMundo.java
+++ holaMundo.java
@@ -1,5 +1,7 @@
 public class holaMundo {
     public static void main(String [] args){
         System.out.println(">>>> Hola Mundo");
+        Vendedor objetoVendedor = new Vendedor(1,"Juan");
+        System.out.println(objetoVendedor.toString());
     }
 }
